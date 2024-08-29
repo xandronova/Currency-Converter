0bc14afb821597d1d95ce891f520ee3abf6afa86
İbrahim Üstüner <ibrahim.ustuner@gazi.edu.tr>
2024-08-29 10:16:44 +0300

    REST API eklendi.

--- flask/app.py
+++ flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import requests
 from config import APİ_KEY
 
@@ -13,8 +13,6 @@ def convert():
     ilk_doviz = request.form['ilk_doviz'].upper()
     cevrilen_doviz = request.form['cevrilen_doviz'].upper()
     miktar = float(request.form['miktar'])
-
-    # API'den veri çekiliyor.
     url = f"http://data.fixer.io/api/latest?access_key={APİ_KEY}"
     yanit = requests.get(url)
     data = yanit.json()
@@ -26,6 +24,23 @@ def convert():
     miktar = round(miktar, 5)
 
     return render_template('index.html', miktar=miktar, cevrilen_doviz=cevrilen_doviz)
+@app.route('/restapi', methods=['GET'])
+def convert2():
+    ana_para_birimi = request.args.get('from')
+    donusturulecek_para_birimi = request.args.get('to')
+    miktar = float(request.args.get('amount'))
+    response = requests.get(f"http://data.fixer.io/api/latest?access_key={APİ_KEY}")
+    data = response.json()
+    rate = data['rates'].get(donusturulecek_para_birimi)
+    donusturulmus_miktar = rate * miktar
+    return jsonify({
+        'İlk para birimi': ana_para_birimi,
+        'Dönüştürülecek para birimi': donusturulecek_para_birimi,
+        'Miktar': miktar,
+        'Dönüştürülmüş miktar': donusturulmus_miktar,
+        'rate': rate
+    
+    })
 @app.route('/about')
 def about():
      return render_template('about.html')
